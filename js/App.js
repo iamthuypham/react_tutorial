af9cde46144503bb8284206d153350dce679ed9c
Thuy Pham <tpham045@hotmail.com>
2016-12-15 01:58:05 +0000

    Start

--- js/App.js
+++ js/App.js
@@ -1,38 +1,20 @@
-import React from 'react'
-import { Match } from 'react-router'
-import { Provider } from 'react-redux'
-import store from './store'
-import AsyncRoute from './AsyncRoute'
-import preload from '../public/data.json'
-if (global) {
-  global.System = { import () {} }
-}
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <div className='app'>
-        <Match
-          exactly
-          pattern='/'
-          component={(props) => <AsyncRoute props={props} loadingPromise={System.import('./Landing')} />}
-        />
-        <Match
-          pattern='/search'
-          component={(props) => {
-            return <AsyncRoute props={Object.assign({shows: preload.shows}, props)} loadingPromise={System.import('./Search')} />
-          }}
-        />
-        <Match
-          pattern='/details/:id'
-          component={(props) => {
-            const show = preload.shows.filter((show) => props.params.id === show.imdbID)
-            return <AsyncRoute props={Object.assign({show: show[0]}, props)} loadingPromise={System.import('./Details')} />
-          }}
-        />
-      </div>
-    </Provider>
+var MyTitle = React.createClass({
+  render() {
+    return (
+      div(null,
+        h1(null, 'Check out this other thing.')
+      )
+    )
+  }
+})
+var MyFirstComponent = (
+  div(null,
+  React.createElement(MyTitle, null)
+    
   )
-}
+)
 
-export default App
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
